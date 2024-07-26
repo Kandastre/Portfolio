8bb83378bc479496dca2c15bfed8261ae1895b73
mehdi <mehdi.benouar@epitech.eu>
2024-07-26 13:35:13 +0200

    add legend

--- portfolio/src/app/ShowProjet.tsx
+++ portfolio/src/app/ShowProjet.tsx
@@ -7,10 +7,16 @@ const ShowProjet: React.FC = () => {
         <div className='projet'>
           <h2 className='titleProjet'>My Twitter</h2>
           <img src="/twitter.png" alt="Project Image" />
+          <p className='titleProjet'>
+            Réplique de l'application web X (anciennement Twitter) en React/Laravel
+          </p>
         </div>
         <div className='projet'>
           <h2 className='titleProjet'>My Spotify</h2>
           <img src="/spotify.png" alt="Project Image" />
+          <p className='titleProjet'>
+          Réplique de l'application web Spotify en React/Docker
+          </p>
         </div>
       </div>
     </div>
