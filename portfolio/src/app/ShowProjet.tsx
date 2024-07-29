e1ccf12786c55ea7751a910ef53f67f32c7f879f
Kanda <151519767+Kandastre@users.noreply.github.com>
2024-07-29 15:37:54 +0200

    Update ShowProjet.tsx

--- portfolio/src/app/ShowProjet.tsx
+++ portfolio/src/app/ShowProjet.tsx
@@ -8,14 +8,14 @@ const ShowProjet: React.FC = () => {
           <h2 className='titleProjet'>My Twitter</h2>
           <img src="/twitter.png" alt="Project Image" />
           <p className='titleProjet'>
-            Réplique de l'application web X (anciennement Twitter) en React/Laravel
+            Réplique de l&apos;application web X (anciennement Twitter) en React/Laravel
           </p>
         </div>
         <div className='projet'>
           <h2 className='titleProjet'>My Spotify</h2>
           <img src="/spotify.png" alt="Project Image" />
           <p className='titleProjet'>
-          Réplique de l'application web Spotify en React/Docker
+            Réplique de l&apos;application web Spotify en React/Docker
           </p>
         </div>
       </div>
