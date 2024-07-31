aed54a51296e5d0ba9d77c088dfad84e67c9cd85
Kanda <151519767+Kandastre@users.noreply.github.com>
2024-07-31 20:33:01 +0200

    Update ShowProjet.tsx

--- portfolio/src/app/ShowProjet.tsx
+++ portfolio/src/app/ShowProjet.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Image from 'next/image';
 
 const ShowProjet: React.FC = () => {
   return (
@@ -6,14 +7,14 @@ const ShowProjet: React.FC = () => {
       <div className='ProjetContainer'>
         <div className='projet'>
           <h2 className='titleProjet'>My Twitter</h2>
-          <img src="/twitter.png" alt="Project Image" />
+          <Image src="/twitter.png" alt="Project Image" width={500} height={300} />
           <p className='titleProjet'>
             Réplique de l&apos;application web X (anciennement Twitter) en React/Laravel
           </p>
         </div>
         <div className='projet'>
           <h2 className='titleProjet'>My Spotify</h2>
-          <img src="/spotify.png" alt="Project Image" />
+          <Image src="/spotify.png" alt="Project Image" width={500} height={300} />
           <p className='titleProjet'>
             Réplique de l&apos;application web Spotify en React/Docker
           </p>
